3a9ee60590fa5d06b9282a39870d610dcbc260d3
alan yen <alan830719@gmail.com>
2022-02-12 23:21:48 +0800

    try fix Module not found: Error: Can't resolve 'ethers' in '/opt/build/repo/src'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { ethers,utils } from "ethers";
+import { ethers , utils } from "ethers";
 import abi from "./contract/legacyProject.json";
 
 function App() {
